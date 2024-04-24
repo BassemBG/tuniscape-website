b97e2688a84e548cb191d16c512190fde6d9fbb4
BassemBG <bgbassem8@gmail.com>
2024-04-24 11:56:10 +0100

    changes backend url to https, since we made the website secured in safozi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://node16861-env-5845845.my.p4d.click:2083/api/',
-  apiUrlGetImage: 'http://node16861-env-5845845.my.p4d.click:2083/api/getimage/',
+  apiBaseUrl: 'https://node16861-env-5845845.my.p4d.click:2083/api/',
+  apiUrlGetImage:
+    'https://node16861-env-5845845.my.p4d.click:2083/api/getimage/',
 };
